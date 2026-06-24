30d0aa0ca9e879844b3d7c1bbc6b2d9f8707f406
chenshuai18 <chenshuai18@xiaomi.com>
2026-06-24 10:58:10 +0800

    Remove force-aware board guidance path

--- TFAC_V5/tac_quality_energy/__init__.py
+++ TFAC_V5/tac_quality_energy/__init__.py
@@ -4,11 +4,6 @@ from .model import DistilledTacQualityEnergy, TASK_TO_ID
 from .board_proxy_energy import BoardProxyEnergyRuntime
 from .foresight_bridge import ForesightBridgeConfig, ForesightTacQualityBridge, SyntheticLatentForesight
 from .force_band_runtime import ForceBandTacQualityEnergy, ForceBandTacQualityEnergyRuntime
-from .force_aware_guidance_runtime import (
-    ForceAwareBoardGuidanceAdapter,
-    ForceAwareForesightGuidanceRuntime,
-    ForceAwareScoreWeights,
-)
 from .insertion_runtime import InsertionRiskScorerRuntime
 from .proxy_features import action_proxy_features_torch, marker_proxy_features_torch
 from .ptg_proxy_runtime import PTGProxyScorerV2Runtime
@@ -23,9 +18,6 @@ __all__ = [
     "BoardProxyEnergyRuntime",
     "ForceBandTacQualityEnergy",
     "ForceBandTacQualityEnergyRuntime",
-    "ForceAwareScoreWeights",
-    "ForceAwareForesightGuidanceRuntime",
-    "ForceAwareBoardGuidanceAdapter",
     "PTGProxyScorerV2Runtime",
     "InsertionRiskScorerRuntime",
     "ForesightBridgeConfig",
